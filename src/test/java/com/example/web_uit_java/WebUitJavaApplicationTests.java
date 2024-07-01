9d3438658ba9612c9fa5003d86bbab6588c4d29f
hvupham <21522804@gm.uit.edu.vn>
2024-07-01 14:59:17 +0700

    create project

---
+++ src/test/java/com/example/web_uit_java/WebUitJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_uit_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebUitJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
